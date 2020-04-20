b2ad00b5530b7dde0779ea2ad5931e973b8b4bcd
Victor <hkchung@ucsc.edu>
2020-04-19 18:05:11 -0700

    Point breakdown comments added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+//Name: Victor Chung
+//Points Breakdown:
+//  Implement a simultaneous two-player mode (50) [2p mode]
+//  Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (50) [Vampires]
+//  If (one of those don't count^):
+//      Create and implement a new weapon (w/ new behavior and graphics) (25) [yes]
+//      Implement mouse control for player movement and mouse click to fire (25) [though mouse click is to slow down, not fire :c]
+//      Implement a new timing/scoring mechanism that adds time to the clock for successful hits (25) [health system instead of scoring]
+//      Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (25) [I drew everything using photoshop, piskel to help me put all the art into a spritesheet]
+//      Implement an alternating two-player mode (25) [there is a 2p mode, I hope this count if the S-tier one doesn't]
+//      Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (25) [Echo is fast as lightning]
+
+//Comments:
+//  The music and sound buffers after replaying the game a few time. (after 5th replays)
+//  This isn't a problem if you restart the page but I really don't know how to fix it.
+//  
+//  I had a lot of fun making this, I hope you enjoy it, and thank you for playing :)
 let config = {
     type: Phaser.AUTO,
     scale: {
